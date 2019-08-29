83d746e217f77f9dd474b0fb0ecdc5f0644d6134
Shestakov Anton <rooksgc@gmail.com>
2019-08-29 12:49:51 +0300

    Header component tests was added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,7 +27,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const Header = props => {
+export const Header = props => {
   const classes = useStyles();
   const { isLoggedin, logoutRequest } = props;
 
@@ -35,7 +35,11 @@ const Header = props => {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
+          <Typography
+            variant="h6"
+            className={classes.title}
+            data-testid="header-text"
+          >
             Loft Taxi
           </Typography>
           <Link to="/map" className={classes.link}>
--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,9 +1,52 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Header from './Header';
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import ConnectedHeader, { Header } from './Header';
+import renderer from 'react-test-renderer';
+import { cleanup, fireEvent, render } from '@testing-library/react';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Header />, div);
-  ReactDOM.unmountComponentAtNode(div);
+afterEach(cleanup);
+
+describe('Тест компонента Header', () => {
+
+  describe('Рендер компонента', () => {
+    it('Рендер проходит без ошибок', () => {
+      const container = document.createElement('div');
+      ReactDOM.render(<BrowserRouter><Header /></BrowserRouter>, container);
+      ReactDOM.unmountComponentAtNode(container);
+    });
+  });
+
+  describe('Состояние компонента', () => {
+    it('Должен содержать элемент с текстом "Loft Taxi"', () => {
+      const { getByTestId } = render(<BrowserRouter><Header /></BrowserRouter>);
+      const element = getByTestId('header-text');
+      expect(element.tagName).toBe('H6');
+      expect(element.innerHTML).toBe('Loft Taxi');
+    });
+
+    // it('Должен содержать кнопки "Карта", "Профиль" и "Войти", если пользователь не залогинился', () => {
+      // const { getByTestId } = render(<Header />);
+      // const element = getByTestId('header-text');
+      // expect(element.tagName).toBe('H6');
+      // expect(element.innerHTML).toBe('Loft Taxi');
+    // });
+  });
+
+  // describe('Поведение компонента', () => {
+  //   it('Клик по кнопке increment увеличивает значение счетчика на 1', () => {
+  //     const { getByText, getByTestId } = render(<Counter />);
+  //     fireEvent.click(getByText("Increment"));
+  //     const counter = getByTestId('counter');
+  //     expect(counter.innerHTML).toBe('1');
+  //   });
+  
+  //   it('Клик по кнопке decrement уменьшает значение счетчика на 1', () => {
+  //     const { getByText, getByTestId } = render(<Counter />);
+  //     fireEvent.click(getByText("Decrement"));
+  //     const counter = getByTestId('counter');
+  //     expect(counter.innerHTML).toBe('-1');
+  //   });
+  // });
+
 });
